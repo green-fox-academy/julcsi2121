49926f7a97dcc3d8cf8ea12a5f47c9baa06e6d5f
julcsi2121 <julcsi2121@gmail.com>
2019-04-03 12:42:46 +0200

    week1-day3 HelloOthers done

---
+++ week-01/day-3/HelloOthers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello, Bence!" << std::endl;
+    std::cout << "Hello, Anna!" << std::endl;
+    std::cout << "Hello, Kata!" << std::endl;
+
+    return 0;
+}

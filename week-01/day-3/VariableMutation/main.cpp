44303687c2a376aae7ce6987163627b42073d893
julcsi2121 <julcsi2121@gmail.com>
2019-04-03 14:17:37 +0200

    week1-day3 VariableMutation done

---
+++ week-01/day-3/VariableMutation/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int a = 3;
+    // make the "a" variable's value bigger by 10
+    a += 10;
+
+    std::cout << a << std::endl;
+
+    int b = 100;
+    // make b smaller by 7
+    b -= 7;
+
+    std::cout << b << std::endl;
+
+    int c = 44;
+    // please double c's value
+    c *= 2;
+
+    std::cout << c << std::endl;
+
+    int d = 125;
+    // please divide by 5 d's value
+    d /= 5;
+
+    std::cout << d << std::endl;
+
+    int e = 8;
+    // please cube of e's value
+    e *= e;
+
+    std::cout << e << std::endl;
+
+    int f1 = 123;
+    int f2 = 345;
+    // tell if f1 is bigger than f2 (print as a boolean)
+    bool fResult = f1 > f2;
+    std::cout << fResult << std::endl;
+
+    int g1 = 350;
+    int g2 = 200;
+    // tell if the double of g2 is bigger than g1 (print as a boolean)
+    bool gResult = g2 > g1;
+    std::cout << gResult << std::endl;
+
+    int h = 135798745;
+    // tell if it has 11 as a divisor (print as a boolean)
+    bool hResult = h % 11 == 0;
+    std::cout << hResult << std::endl;
+
+    int i1 = 10;
+    int i2 = 3;
+    // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+    bool iResult = (i1 > i2*i2) && (i1 < i2*i2*i2);
+    std::cout << iResult << std::endl;
+
+    int j = 1521;
+    // tell if j is dividable by 3 or 5 (print as a boolean)
+    bool jResult = (j % 3 == 0) || (j % 5 == 0);
+    std::cout << jResult << std::endl;
+
+    return 0;
+}

995a522c0b3272aa63a37fb82301562223a4631b
julcsi2121 <julcsi2121@gmail.com>
2019-04-03 17:48:42 +0200

    week1-day3 DrawDiagonal done

---
+++ week-01/day-3/DrawDiagonal/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%
+    // %%  %
+    // % % %
+    // %  %%
+    // %%%%%
+    //
+    // The square should have as many lines as the number was
+
+    int number;
+    std::cout << "Write your number" << std::endl;
+    std::cin >> number;
+
+    //első sor
+    for (int i = 1; i <= number ; ++i) {
+        std::cout << "*";
+    }
+    std::cout << std::endl;
+
+    //közepe
+    for (int j = 1; j <= number-2 ; ++j) {
+        std::cout << "*";
+        for (int k = 0; k < j; ++k) {
+            if (k == 0) {
+                continue;
+            } else {
+                std::cout << " ";
+        }
+        }
+        std::cout << "*";
+        for (int l = number-j-2; l >=0  ; --l) {
+            if (l == 0) {
+                continue;
+            } else {
+                std::cout << " ";
+            }
+
+        }
+        std::cout << "*" << std::endl;
+
+    }
+
+    //utolsó
+    for (int i = 1; i <= number ; ++i) {
+        std::cout << "*";
+    }
+
+    return 0;
+}
--- week-01/day-3/DrawTriangle/main.cpp
+++ week-01/day-3/DrawTriangle/main.cpp
@@ -12,19 +12,16 @@ int main(int argc, char* args[]) {
     //
     // The triangle should have as many lines as the number was
     int number;
-    int i = 0;
+    int i = 1;
     std::cout << "Write your number please" << std::endl;
     std::cin >> number;
 
-    // two counters: i and j. i counts lines, j counts number of *-s
-    // In order to execute both loops, i should be always +1 bigger than j.
-
-    while(i != number) {
-        i++;
-        for (int j = 0; j != i; j++) {
+    while(i <= number) {
+        for (int j = 1; j <= i; j++) {
             std::cout << "*";
         }
         std::cout << std::endl;
+        i++;
     }
 
     return 0;

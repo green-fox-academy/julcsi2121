4ae3742bb83f93cbff9288853ebf9cd6e9fd60dd
julcsi2121 <julcsi2121@gmail.com>
2019-04-04 13:20:21 +0200

    week1-day4 ChangeElement done, SumElement was only modified by accident

---
+++ week-01/day-4/ChangeElement/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `s`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+    int s[6] = {1, 2, 3, 8, 5, 6};
+    s[3] = 4;
+    std::cout << "4th elementh is changed now, it's value is " << s[3] << std::endl;
+    return 0;
--- week-01/day-4/SumElements/main.cpp
+++ week-01/day-4/SumElements/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* args[]) {
     //   with the following content: `[54, 23, 66, 12]`
     // - Print the sum of the second and the third element
     int r[4] = {54, 23, 66, 12};
-    std::cout << "Sum of the 2nd & 3rd element is: " << r[1] + r[2] << std::endl;
+    std::cout << "Sum of 2nd & 3rd element is: " << r[1] + r[2] << std::endl;
+
     return 0;
 }

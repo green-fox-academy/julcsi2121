cf85cb7c26e0c86daf7bde58d1ebc62c42cc820f
julcsi2121 <julcsi2121@gmail.com>
2019-04-05 16:28:19 +0200

    SortThatList done

--- week-01/day-5/SortThatList/main.cpp
+++ week-01/day-5/SortThatList/main.cpp
@@ -2,64 +2,86 @@
 #include <string>
 
 void bubbleSort(int list[], int size);
+void reverseBubbleSort(int list[], int size);
+bool choose(bool value, int list[], int size);
 
 //  Create a function that takes a list of numbers as parameter
 //  Don't forget you have to pass the size of the list as a parameter as well
 //  Returns a list where the elements are sorted in ascending numerical order by using bubble sort
 //  Make a second boolean parameter, if it's `true` sort that list descending
 
-/* ---Pesudo-code-----
-int main(int argc, char* args[])
-{
-    adat bekérő stuff
-    fgv hívás
-
-    return 0;
-}
-
-bubble sort növekvő (lista, lista méret):
-sort:
-while lista i nagyobb mint i+1   // ellenőrző ciklus: amíg a lista i eleme nagyobb, mint a kövi
-    for lista méret:                 // megy egy kört
-        if i nagyobb mint i+1        // utolsó elem??????
-            csere
-    i++                              // végigellenőrzi az egész listát
-
-print:
-bubble sort csökkenő: csak a printnek elég változnia
-    ha true, akkor sima print
-    ha false, akkor reverse print
-*/
-
  int main(int argc, char* args[])
 {
 
-    int list[6] = {250, 4, 3, 5, 43, 1};
-    int size = 6;
-    bubbleSort(list, size);
+    int size;
+    std::cout << "Give me the size of your array" << std::endl;
+    std::cin >> size;
+    int list[size];
+    std::cout << "Now give the elements of the array one by one" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << i+1 << ". elementh: ";
+        std::cin >> list[i];
+    }
+    bool descendingOrder;
+    std::cout << "Do you want descending order? Please answer with 1 for true and 0 for false.";
+    std::cin >> descendingOrder;
+
+    choose(descendingOrder, list, size);
 
     return 0;
 }
 
 void bubbleSort(int list[], int size) {
      //sort:
-     int i;
-     while (list[i] > list[i +1]) {
-        for (int j = 0; j < size ; ++j) {
-            std::cout << "Before: " << list[j] << " and " << list[j + 1] << std::endl;
-            if (j > (j + 1)) {
-                int temp;
-                list[j] = temp;
-                std::cout << "temp: " << temp << std::endl;
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
+    for (int l = 0; l < size; ++l) {
+         std::cout << "This is the start of an outer cycle" << std::endl;
+        for (int j = 0; j < size-1 ; ++j) {
+            if (list[j] > list[j + 1]) {
+                list[j] = list[j] - list[j + 1];
+                list[j + 1] = list[j] + list[j + 1];
+                list[j] = list[j + 1] - list[j];
             }
-            std::cout << "After: " << list[j] << " and " << list[j + 1] << std::endl;
+            for (int k = 0; k < size; ++k) {
+                std::cout << list[k] << " ";
+            }
+            std::cout << std::endl;
         }
-        i++;
      }
+    //print:
      std::cout << "Sorted list: " << std::endl;
      for (int k = 0; k < size ; ++k) {
         std::cout << list[k] << std::endl;
     }
  }
+
+void reverseBubbleSort(int list[], int size) {
+    //sort:
+    for (int l = 0; l < size; ++l) {
+        std::cout << "This is the start of an outer cycle" << std::endl;
+        for (int j = 0; j < size-1 ; ++j) {
+            if (list[j] < list[j + 1]) {
+                list[j] = list[j] - list[j + 1];
+                list[j + 1] = list[j] + list[j + 1];
+                list[j] = list[j + 1] - list[j];
+            }
+            for (int k = 0; k < size; ++k) {
+                std::cout << list[k] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    //print:
+    std::cout << "Sorted list: " << std::endl;
+    for (int k = 0; k < size ; ++k) {
+        std::cout << list[k] << std::endl;
+    }
+}
+
+bool choose(bool value, int list[], int size)
+{
+     if (value == true){
+         reverseBubbleSort(list, size);
+     } else{
+         bubbleSort(list, size);
+     }
+}

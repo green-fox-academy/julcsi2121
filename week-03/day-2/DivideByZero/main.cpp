bb12bd30a8f77e211e2fc41c4f79ed6cd0665786
julcsi2121 <julcsi2121@gmail.com>
2019-04-16 11:40:37 +0200

    DivideByZero done

---
+++ week-03/day-2/DivideByZero/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+void divideWithTen(int number);
+
+int main() {
+    // Create a function that takes a number
+    // divides ten with it,
+    // and prints the result.
+    // It should print "fail" if the parameter is 0
+    divideWithTen(0);
+
+    return 0;
+}
+
+void divideWithTen(int number)
+{
+    try {
+        if(number == 0) {
+            throw std::string("fail");
+        } else {
+            std::cout << number / 10 << std::endl;
+        }
+
+    } catch (std::string& error) {
+        std::cout << error << std::endl;
+    }
+
+}

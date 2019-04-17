ccc5c3c803d7451721b926107a5e846f44f2d035
julcsi2121 <julcsi2121@gmail.com>
2019-04-17 15:08:33 +0200

    week-3 day-3 Pokemon done

---
+++ week-03/day-3/Pokemon/Pokemon.h
@@ -0,0 +1,20 @@
+//
+// Created by Varga Júlia on 2019-04-17.
+//
+#ifndef POKEMON_POKEMON_H
+#define POKEMON_POKEMON_H
+
+#include <iostream>
+#include <string>
+
+class Pokemon {
+public:
+    Pokemon(const std::string& name, const std::string& type, const std::string& effectiveAgainst);
+    bool isEffectiveAgainst(Pokemon anotherPokemon);
+
+    std::string _name;
+    std::string _type;
+    std::string _effectiveAgainst;
+};
+
+#endif //POKEMON_POKEMON_H

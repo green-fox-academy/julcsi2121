b541fb611185b9324af49d6bb8f879a29297963a
julcsi2121 <julcsi2121@gmail.com>
2019-04-23 13:27:04 +0200

    week4-day1 GreenFoxOrganization subtask Student done

--- week-04/day-2/GreenFoxOrganization/Student.h
+++ week-04/day-2/GreenFoxOrganization/Student.h
@@ -5,9 +5,28 @@
 #ifndef GREENFOXORGANIZATION_STUDENT_H
 #define GREENFOXORGANIZATION_STUDENT_H
 
+#include <iostream>
+#include <string>
+#include "Person.h"
 
-class Student {
 
+class Student : public Person {
+public:
+    Student();
+    Student(std::string name, int age, Gender gender, std::string previousOrganization);
+
+    std::string getPreviousOrganization() const;
+    void setPreviousOrganization(std::string previousOrganization);
+    int getSkippedDays() const;
+    void setSkippedDays(int skippedDays);
+
+    void getGoal();
+    void introduce();
+    void skipDays(int numberOfDays);
+
+private:
+    std::string _previousOrganization;
+    int _skippedDays;
 };
 
 
--- week-04/day-2/GreenFoxOrganization/main.cpp
+++ week-04/day-2/GreenFoxOrganization/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "Person.h"
+#include "Student.h"
 
 int main() {
-    Person jane;
+    //Person jane;
     Person mark("Mark", 28, MALE);
-    jane.introduce();
-    jane.getGoal();
+    //jane.introduce();
+    //jane.getGoal();
     mark.introduce();
+    Student jane;
+    jane.skipDays(5);
+    jane.introduce();
+    Student david("David", 22, MALE, "Elte");
+    david.skipDays(2);
+    david.introduce();
+    david.getGoal();
     return 0;
 }

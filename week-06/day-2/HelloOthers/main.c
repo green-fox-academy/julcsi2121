25465ecbbae93307464583e8ea13546964dd085f
julcsi2121 <julcsi2121@gmail.com>
2019-05-14 11:45:34 +0200

    week6-day2 HelloOthers done

---
+++ week-06/day-2/HelloOthers/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, Kata!\n");
+    printf("Hello, Beno!\n");
+    printf("Hello, Viktor!\n");
+
+    return 0;
+}

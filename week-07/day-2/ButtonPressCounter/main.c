f96372e7e6ed1f53da8beb15cb468fc286fbfebf
julcsi2121 <julcsi2121@gmail.com>
2019-05-21 16:37:38 +0200

    week7-day2 ButtonPressCounter refactored

--- week-07/day-2/ButtonPressCounter/main.c
+++ week-07/day-2/ButtonPressCounter/main.c
@@ -1,35 +1,32 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
+
+/*
+ * Your program should start in a waiting state. After the user presses (and releases)
+ * the button 5 times, turn on the user LED and put your program into a state
+ * in which it is not reacting to any further button presses.
+ */
+
 
 int main(void)
 {
-	HAL_Init();
-	BSP_LED_Init(LED1);
-	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
-	int number_of_button_presses = 0;
-
-	while(1){
-		if(BSP_PB_GetState(BUTTON_KEY) == 1) {
-			while(BSP_PB_GetState(BUTTON_KEY) == 1) {
-
-			}
-			number_of_button_presses++;
-		}
-
-		if(number_of_button_presses == 5) {
-			BSP_LED_On(LED1);
-		}
-	}
+    HAL_Init();
+    BSP_LED_Init(LED1);
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+    int button_down = 0;
+    int number_of_button_presses = 0;
+    
+    while(1){
+        if(BSP_PB_GetState(BUTTON_KEY) == 1) {
+            button_down = 1;
+        }
+        
+        if(BSP_PB_GetState(BUTTON_KEY) == 0 && button_down == 1) {
+            button_down = 0;
+            number_of_button_presses++;
+            if (number_of_button_presses == 5) {
+                BSP_LED_On(LED1);
+            }
+        }
+    }
 }

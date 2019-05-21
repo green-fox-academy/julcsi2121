6a150cce5ae2161b3b0872450251c3806b4da61c
julcsi2121 <julcsi2121@gmail.com>
2019-05-21 16:31:40 +0200

    week7-day2 BlockingTheExecution refactored

--- week-07/day-2/BlockingTheExecution/main.c
+++ week-07/day-2/BlockingTheExecution/main.c
@@ -1,34 +1,25 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
+
+/*
+ * Make the green user LED flash with a frequency of 2 Hz but do not start until the blue user
+ * button is pushed. Once it is pushed the LED should be blinking forever.
+ */
 
 int main(void)
 {
-	HAL_Init();
-	BSP_LED_Init(LED1);
-	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
-	int activated_once = 0;
-
-	while(1){
-		if(BSP_PB_GetState(BUTTON_KEY) == 1) {
-			activated_once = 1;
-		}
-		if(activated_once == 1) {
-			BSP_LED_On(LED1);
-			HAL_Delay(500);
-			BSP_LED_Off(LED1);
-			HAL_Delay(500);
-		}
-	}
+    HAL_Init();
+    BSP_LED_Init(LED1);
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+    int activated_once = 0;
+    
+    while(1){
+        if(BSP_PB_GetState(BUTTON_KEY) == 1) {
+            activated_once = 1;
+        }
+        if(activated_once == 1) {
+            BSP_LED_Toggle(LED1);
+            HAL_Delay(250);
+        }
+    }
 }

b5267c214c2725e9bce8afb90cbcd506a90ed19e
julcsi2121 <julcsi2121@gmail.com>
2019-05-21 16:00:37 +0200

    week7-day2 FlashingLED refactored done

--- week-07/day-2/FlashingLED/main.c
+++ week-07/day-2/FlashingLED/main.c
@@ -1,48 +1,24 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
-
-int main(void)
-{
-	HAL_Init();
-	BSP_LED_Init(LED1);
-	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
-
-	while(1){
-		if(BSP_PB_GetState(BUTTON_KEY) == 1) {
-			BSP_LED_On(LED1);
-			HAL_Delay(250);
-			BSP_LED_Off(LED1);
-			HAL_Delay(250);
-		} else {
-			BSP_LED_Off(LED1);
-		}
-	}
-}
 
 /*
+ * Make the green user LED flash with a frequency of 4 Hz while the blue user pushbutton
+ * is pressed. If the button is not pressed the LED should not be lit.
+ */
+
+
 int main(void)
 {
     HAL_Init();
-
     BSP_LED_Init(LED1);
-
-    while (1) {
-        BSP_LED_On(LED1);
-        HAL_Delay(500);
-        BSP_LED_Off(LED1);
-        HAL_Delay(500);
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+    
+    while(1){
+        if(BSP_PB_GetState(BUTTON_KEY) == 1) {
+            BSP_LED_On(LED1);
+            HAL_Delay(125);
+            BSP_LED_Off(LED1);
+            HAL_Delay(125);
+        }
     }
 }
-*/

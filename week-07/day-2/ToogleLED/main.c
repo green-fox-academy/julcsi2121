0e076963be6f01463780d7c4cfa6a51f0a1f9c73
julcsi2121 <julcsi2121@gmail.com>
2019-05-21 16:19:49 +0200

    week7-day2 ToogleLED refactored done

--- week-07/day-2/ToogleLED/main.c
+++ week-07/day-2/ToogleLED/main.c
@@ -1,36 +1,27 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
+
+/*
+ * Toggle the green user LED with the blue user button.
+ * A push should turn on the LED if it is turned off and
+ * it should turn the LED off if it is turned on.
+ */
+
 
 int main(void)
 {
-	HAL_Init();
-	BSP_LED_Init(LED1);
-	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
-	int on = 0;
-
-	while(1){
-		if(BSP_PB_GetState(BUTTON_KEY) == 1 && on == 0) {
-			while(BSP_PB_GetState(BUTTON_KEY) == 1) {
-			}
-			BSP_LED_On(LED1);
-			on = 1;
-		} else if (BSP_PB_GetState(BUTTON_KEY) == 1 && on == 1) {
-			while(BSP_PB_GetState(BUTTON_KEY) == 1) {
-			}
-			BSP_LED_Off(LED1);
-			on = 0;
-		}
-	}
+    HAL_Init();
+    BSP_LED_Init(LED1);
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+    int button_down = 0;
+    
+    while(1){
+        if (BSP_PB_GetState(BUTTON_KEY) == 1 && button_down == 0) {
+            button_down = 1;
+            BSP_LED_Toggle(LED1);
+        }
+        if (BSP_PB_GetState(BUTTON_KEY) == 0) {
+            button_down = 0;
+        }
+    }
 }

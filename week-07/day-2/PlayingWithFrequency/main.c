7fb7c47278cc03751e8212026eb1cd1b04f26e0e
julcsi2121 <julcsi2121@gmail.com>
2019-05-21 17:18:38 +0200

    week7-day2 PlayingWithFrequency refactored

--- week-07/day-2/PlayingWithFrequency/main.c
+++ week-07/day-2/PlayingWithFrequency/main.c
@@ -1,93 +1,35 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
-			
+
+/*
+ * Flash the user LED with a changing frequency.
+ * The frequency should change every 3 periods (on and off) and the steps should be the following:
+ * 0.5 Hz ➡ 1 Hz ➡ 2 Hz ➡ 4 Hz ➡ 2 Hz ➡ 1 Hz ➡ 0.5 Hz ➡ ...
+ */
+
 
 int main(void)
 {
-	HAL_Init();
-	BSP_LED_Init(LED1);
-	int counter = 0;
-	int set_frequency = 0;
-
-	while(1){
-		if (set_frequency % 6 == 0){
-				BSP_LED_On(LED1);
-				HAL_Delay(250);
-				BSP_LED_Off(LED1);
-				HAL_Delay(250);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-		}
-		if (set_frequency % 6 == 1) {
-				BSP_LED_On(LED1);
-				HAL_Delay(500);
-				BSP_LED_Off(LED1);
-				HAL_Delay(500);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-
-		}
-		if (set_frequency % 6 == 2) {
-				BSP_LED_On(LED1);
-				HAL_Delay(1000);
-				BSP_LED_Off(LED1);
-				HAL_Delay(1000);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-
-		}
-		if (set_frequency % 6 == 3) {
-				BSP_LED_On(LED1);
-				HAL_Delay(2000);
-				BSP_LED_Off(LED1);
-				HAL_Delay(2000);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-		}
-		if (set_frequency % 6 == 4) {
-				BSP_LED_On(LED1);
-				HAL_Delay(1000);
-				BSP_LED_Off(LED1);
-				HAL_Delay(1000);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-		}
-		if (set_frequency % 6 == 5) {
-				BSP_LED_On(LED1);
-				HAL_Delay(500);
-				BSP_LED_Off(LED1);
-				HAL_Delay(500);
-				counter++;
-				if(counter == 3) {
-					counter = 0;
-					set_frequency++;
-				}
-		}
-	}
+    HAL_Init();
+    BSP_LED_Init(LED1);
+    int state = 0;
+    int set_frequency = 2000;
+    
+    while(1){
+        for (int i = 0; i < 6; i++) {
+            BSP_LED_Toggle(LED1);
+            HAL_Delay(set_frequency);
+        }
+        if (state == 0) {
+            set_frequency /= 2;
+            if (set_frequency == 250) {
+                state = 1;
+            }
+        } else {
+            set_frequency *= 2;
+            if (set_frequency == 2000) {
+                state = 0;
+            }
+        }
+    }
 }
